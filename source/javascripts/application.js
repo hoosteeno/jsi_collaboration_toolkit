00057c89b4722b42a82c0cc0adbd412c63a2ec65
Steve Bailey <icegulch@gmail.com>
2012-04-03 16:22:08 -0600

    javascript ui stuff

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -3,34 +3,43 @@
 
 jQuery(document).ready(function(){
 
-  jQuery('#tabs, #subtabs').tabs();
+  jQuery('#chapters').tabs({ fx: { opacity: 'toggle' } });
+  jQuery('#sections').tabs();
 
-  // var subtabs = jQuery('#subtabs').tabs({ fx: { opacity: 'toggle' } });
+  var deep_link = window.location.hash.substring(1).split('/');
+  if (deep_link != null) {
+   if (deep_link[0] != null) {
+     jQuery(chapters).tabs('select', deep_link[0]);
+   }
+   if (deep_link[1] != null) {
+     jQuery(sections).tabs('select', deep_link[1]);
+   }
+  }
 
-  // jQuery("#subtabs .ui-tabs-panel").each(function(i){
-  //   console.log(i+"straight up i");
 
-  //   var totalSize = jQuery("#subtabs .ui-tabs-panel").size() - 1;
-  //   console.log(totalSize+"total size");
+  // jQuery('#sections .ui-tabs-panel').each(function(i){
+  //   var lastinarray = jQuery("#sections .ui-tabs-panel").size() - 1;
+  //   var current = i + 1;
 
-  //   if (i != totalSize) {
-  //       next = i + 1;
-  //       console.log(next+"next");
-  //       jQuery(this).append("<a href='#' class='next-tab mover' rel='" + next + "'>Next Page &#187;</a>");
+  //   // if "i" is not last in array
+  //   if (i != lastinarray) {
+  //     // next equals current plus one
+  //     next = current + 1;
+  //     jQuery(this).append("<a href='#section"+next+"' class='next-section section-link' rel='" + next + "'>Next Page &#187;</a>");
   //   }
 
+  //   // if "i" is not first in array
   //   if (i != 0) {
-  //       prev = i - 1;
-  //       console.log(prev+"prev");
-  //       jQuery(this).append("<a href='#' class='prev-tab mover' rel='" + prev + "'>&#171; Prev Page</a>");
+  //       prev = current - 1;
+  //       jQuery(this).append("<a href='#section"+prev+"' class='prev-section section-link' rel='" + prev + "'>&#171; Prev Page</a>");
   //   }
-  
+
   // });
 
-  // jQuery('.next-tab, .prev-tab').click(function() { 
-  //   subtabs.tabs('select', jQuery(this).attr("rel"));
-  //   console.log(subtabs);
-  //   return false;
+  // jQuery('.next-section, .prev-section').click(function() {
+  //   jQuery(sections).tabs('select', jQuery(this).attr("href"));
+  //    return false;
   // });
+
 
 });
