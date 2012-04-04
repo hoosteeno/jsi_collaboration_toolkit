96970ffc8a540c01e3d55f168d0554aeecadafb5
hoosteeno <justin@dojo4.com>
2012-04-04 14:31:11 -0600

    added chapters to the mix; moved content into yaml

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,55 +1,66 @@
-/* Application JS: */
+jQuery(function(){
 
+  // this is inside to avoid clobbering $ for other jQuery scripts
+  var jq = jQuery.noConflict();
 
-jQuery(document).ready(function(){
+  jq('#chapters, #sections, #subsections').tabs();
 
-  jQuery('#chapters, #sections').tabs();
-
-  // Exposes deep links
-  var deep_link = window.location.hash.substring(1).split('/');
-  if (deep_link != null) {
-   if (deep_link[0] != null) {
-     jQuery(chapters).tabs('select', deep_link[0]);
-   }
-   if (deep_link[1] != null) {
-     jQuery(sections).tabs('select', deep_link[1]);
-   }
+  // show deep link from URL
+  var deep_link_nodes = window.location.hash.substring(1).split('/');
+  if (deep_link_nodes != null) {
+     
+    if (deep_link_nodes[0] != null) {
+      jq(chapters).tabs('select', deep_link_nodes[0]);
+    }
+    if (deep_link_nodes[1] != null) {
+      jq(sections).tabs('select', deep_link_nodes[1]);
+    }
+    if (deep_link_nodes[2] != null) {
+      jq(subsections).tabs('select', deep_link_nodes[2]);
+    }
   }
 
-  // This sets hashtag on URL
-  jQuery('.ui-tabs-nav li a').click(function(){
-    if (jQuery(this).closest('#sections').size() > 0) {
-      var chapter = jQuery(this).closest('div[id^="chapter"]').attr('id');
-      window.location.hash = '#'+chapter+'/'+jQuery(this).attr('href').substring(1);
+  // add deep link to URL
+  jq('.ui-tabs-nav li a').click(function(){
+    if (jq(this).closest('#subsections').size() > 0) {
+      var chapter = jq(this).closest('div[id^="chapter"]').attr('id');
+      var section = jq(this).closest('div[id^="section"]').attr('id');
+      window.location.hash = '#'+chapter+'/'+section+'/'+jq(this).attr('href').substring(1);
+    }
+    else if (jq(this).closest('#sections').size() > 0) {
+      var chapter = jq(this).closest('div[id^="chapter"]').attr('id');
+      window.location.hash = '#'+chapter+'/'+jq(this).attr('href').substring(1);
     }
     else {
-      window.location.hash = jQuery(this).attr('href');
+      window.location.hash = jq(this).attr('href');
     }
   });
 
-  // jQuery('#sections .ui-tabs-panel').each(function(i){
-  //   var lastinarray = jQuery("#sections .ui-tabs-panel").size() - 1;
+  // jq('#sections .ui-tabs-panel').each(function(i){
+  //   var lastinarray = jq("#sections .ui-tabs-panel").size() - 1;
   //   var current = i + 1;
 
   //   // if "i" is not last in array
   //   if (i != lastinarray) {
   //     // next equals current plus one
   //     next = current + 1;
-  //     jQuery(this).append("<a href='#section"+next+"' class='next-section section-link' rel='" + next + "'>Next Page &#187;</a>");
+  //     jq(this).append("<a href='#section"+next+"' class='next-section section-link' rel='" + next + "'>Next Page &#187;</a>");
   //   }
 
   //   // if "i" is not first in array
   //   if (i != 0) {
   //       prev = current - 1;
-  //       jQuery(this).append("<a href='#section"+prev+"' class='prev-section section-link' rel='" + prev + "'>&#171; Prev Page</a>");
+  //       jq(this).append("<a href='#section"+prev+"' class='prev-section section-link' rel='" + prev + "'>&#171; Prev Page</a>");
   //   }
 
   // });
 
-  // jQuery('.next-section, .prev-section').click(function() {
-  //   jQuery(sections).tabs('select', jQuery(this).attr("href"));
+  // jq('.next-section, .prev-section').click(function() {
+  //   jq(sections).tabs('select', jq(this).attr("href"));
   //    return false;
   // });
 
 
 });
+
+
